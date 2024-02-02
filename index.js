1a1483a8c4e4a06df1d43d3dbe9a5623948efd51
randomtejas345 <tejasrajwagh@gmail.com>
2024-02-02 19:10:29 +0530

    Hide the secret files

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname=dirname(fileURLToPath(import.meta.url))
+const app=express();
+const port=3000;
+
+
+
+app.set("view engine","ejs")
+
+app.use(express.static(__dirname+"/public"))
+
+app.get("/",(req,res)=>{
+    res.render("index")
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
